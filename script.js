cda1eb74f6d1aabbfcd091bc023a0e19462102ce
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2022-01-05 13:36:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello winc academy students")
+
+
+
+/* disable the code
+//this is a large sum
+let vermenigvuldigen = 1230941 * 1823794;
+console.log(vermenigvuldigen);
+
+let delen = 637263 / 54;
+console.log(vermenigvuldigen);
+*/
+
+Without the block comment the calc worked
+with it it doesnt
+
